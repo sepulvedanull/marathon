fc571d0cd2548b635af157c2360a1b78996ca433
trentontri <trentontri@gmail.com>
2018-03-13 15:52:46 -0500

    bug fix server typo

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/marathon_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dgvnwqnawhccop:168b5904636719b5d62c585ca79b821f9d2fd7d0090ed9048cb744530d628538@ec2-54-221-212-15.compute-1.amazonaws.com:5432/d53bb2j3fl7r7l'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
+# Create our database model
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    emal = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
     def __init__(self, email):
         self.email = email
@@ -19,10 +20,12 @@ class User(db.Model):
     def __repr__(self):
         return '<E-mail %r>' % self.email
 
+# Set "homepage" to index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Save e-mail to database and send to success page
 @app.route('/prereg', methods=['POST'])
 def prereg():
     email = None
