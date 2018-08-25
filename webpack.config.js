ff064ed6c92116bfa94f4e4a6c7688390760bc42
Trenton Kennedy <trentontri@gmail.com>
2018-08-25 14:03:22 -0500

    bugfix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,11 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
   entry: path.resolve('./public/runner-performances.js'),
   devtool: 'inline-source-map',
   output: {
     filename: '[name].min.js',
     path: path.resolve(__dirname, './public/dist')
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new UglifyJsPlugin({
-        include: /\.min\.js$/
-      })
-    ]
   },
   devServer: {
     contentBase: './'
