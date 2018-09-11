f24b9d3e6182c9a6ee29c6cebbba351fb8791c51
Trenton Kennedy <trentontri@gmail.com>
2018-09-10 20:32:38 -0500

    refactor node-server folder

--- node-server/readme.md
+++
@@ -1,6 +0,0 @@
-Marathon Visualization
-=====
-
-build ui code: `npm run build`
-run scraper: `python scraper.py`
-run server: `python app.py`
--- server.js
+++ server.js
@@ -1,30 +1,22 @@
-import express from 'express'
-
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
-
-app.listen(4000, () => {
-  console.log('server loaded on http://localhost:4000/')
-});
-
-
-
-/*var path = require('path');
+var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var express = require('express');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 // uncomment after placing your favicon in /client
 //app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
@@ -32,10 +24,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,4 +46,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;*/
+module.exports = app;
