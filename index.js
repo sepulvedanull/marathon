4447a59208fadde11e7624ab6cdbdcb94413f617
Trenton Kennedy <trentontri@gmail.com>
2018-10-21 15:04:36 -0500

    add remaining 2017 mock data

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import marathonRunners from './data-scraper/data/2017/marathon.json'
+import marathon_2017 from './data-scraper/data/2017/marathon.json'
+import half_marathon_2017 from './data-scraper/data/2017/half-marathon.json'
+import tenk_run_2017 from './data-scraper/data/2017/10k-run.json'
+import fivek_run_2017 from './data-scraper/data/2017/5k-run.json'
+
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/api/v1/runners', (req, res) => {
-  res.status(200).send(marathonRunners)
+app.get('/api/2017/marathon', (req, res) => {
+  res.status(200).send(marathon_2017)
+});
+
+app.get('/api/2017/half-marathon', (req, res) => {
+  res.status(200).send(half_marathon_2017)
+});
+
+app.get('/api/2017/10k-run', (req, res) => {
+  res.status(200).send(tenk_run_2017)
+});
+
+app.get('/api/2017/5k-run', (req, res) => {
+  res.status(200).send(fivek_run_2017)
 });
 
 const PORT = 5000;
