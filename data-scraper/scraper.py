939b51be593e5b4756b89b4ac9663ac6cb2068df
Trenton Kennedy <trentontri@gmail.com>
2018-10-21 12:13:30 -0500

    update web scraper to include all 2017 events

--- data-scraper/scraper.py
+++ data-scraper/scraper.py
@@ -5,18 +5,28 @@ import requests
 
 
 def parse_2017_results():
-    marathon2017Events = [
-        'http://www.besttimescct.com/results/marathon-results-by-place-2017.HTML',
-        'http://www.besttimescct.com/results/half-marathon-results-by-place-2017.HTML',
-        'http://www.besttimescct.com/results/10K-results-by-place-2017.HTML',
-        'http://www.besttimescct.com/results/5K-results-by-place-2017.HTML'
-    ]
 
-    fivek_tables = pd.read_html(marathon2017Events[3], header=0)
-    fivek_table = pd.concat(fivek_tables[0:83])
+    marathon_tables = pd.read_html('http://www.besttimescct.com/results/marathon-results-by-place-2017.HTML', header=0)
+    half_marathon_tables = pd.read_html('http://www.besttimescct.com/results/half-marathon-results-by-place-2017.HTML', header=0)
+    tenk_run_tables = pd.read_html('http://www.besttimescct.com/results/10K-results-by-place-2017.HTML', header=0)
+    fivek_run_tables = pd.read_html('http://www.besttimescct.com/results/5K-results-by-place-2017.HTML', header=0)
 
-    with open('./data/fivek.json', 'w') as outfile:
-        outfile.write(fivek_table.to_json(orient='records'))
+    marathon_data = pd.concat(marathon_tables[0:len(marathon_tables)])
+    half_marathon_data = pd.concat(half_marathon_tables[0:len(half_marathon_tables)])
+    tenk_data = pd.concat(tenk_run_tables[0:len(tenk_run_tables)])
+    fivek_data = pd.concat(fivek_run_tables[0:len(fivek_run_tables)])
+
+    with open('./data/2017/marathon.json', 'w') as outfile:
+        outfile.write(marathon_data.to_json(orient='records'))
+
+    with open('./data/2017/half-marathon.json', 'w') as outfile:
+        outfile.write(half_marathon_data.to_json(orient='records'))
+
+    with open('./data/2017/10k-run.json', 'w') as outfile:
+        outfile.write(tenk_data.to_json(orient='records'))
+
+    with open('./data/2017/5k-run.json', 'w') as outfile:
+        outfile.write(fivek_data.to_json(orient='records'))
 
     return
 
