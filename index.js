d8c3f6226784358aaa52913460be06a44c3d91b0
Brandon Thornburg <brandon.thornburg@stjude.org>
2018-10-24 16:27:14 -0500

    The API is now dynamic, loading data on demand instead of at runtime

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import marathon_2017 from './data-scraper/data/2017/marathon.json'
-import half_marathon_2017 from './data-scraper/data/2017/half-marathon.json'
-import tenk_run_2017 from './data-scraper/data/2017/10k-run.json'
-import fivek_run_2017 from './data-scraper/data/2017/5k-run.json'
+import fs from "fs";
+import express from "express";
+import bodyParser from "body-parser";
 
+const app = express();
 
-const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.get("/api/:year/:race", (req, res) => {
+  var dataPath = `./data-scraper/data/${req.params.year}/${
+    req.params.race
+  }.json`;
 
-app.get('/api/2017/marathon', (req, res) => {
-  res.status(200).send(marathon_2017)
+  if (!fs.existsSync(dataPath)) {
+    res.status(422).send("Data file does not exist");
+  } else {
+    var data = require(dataPath);
+    res.status(200).send(data);
+  }
 });
 
-app.get('/api/2017/half-marathon', (req, res) => {
-  res.status(200).send(half_marathon_2017)
-});
-
-app.get('/api/2017/10k-run', (req, res) => {
-  res.status(200).send(tenk_run_2017)
-});
-
-app.get('/api/2017/5k-run', (req, res) => {
-  res.status(200).send(fivek_run_2017)
-});
-
-const PORT = 5000;
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+});
