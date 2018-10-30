236156141a6e69f1687dc08015e6c05a1bba5f1d
trentontri <trentontri@gmail.com>
2018-10-30 14:53:36 -0500

    install react autosuggest

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,44 +1,102 @@
 import React, {Component} from 'react'
 import Autosuggest from 'react-autosuggest'
 
+// Imagine you have a list of languages that you'd like to autosuggest.
+const languages = [
+  {
+    name: 'Chris Raulli'
+  },
+  {
+    name: 'Adam Higham'
+  },
+  {
+    name: 'Brent Corbitt'
+  },
+];
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : languages.filter(lang =>
+      lang.name.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+    <div>
+      {suggestion.name}
+    </div>
+);
+
+
 class SearchBar extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };
   }
 
-
-  onSuggestionsFetchRequested = () => {
-
+  onChange = (event, {newValue}) => {
+    this.setState({
+      value: newValue
+    })
   }
+
+  onSuggestionsFetchRequested = ({value}) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
 
   onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
-  }
 
   render() {
-    const suggestions = ['frank', 'tim', 'bob']
+    const {value, suggestions} = this.state;
+
+    // Autosuggest will pass through all these props to the input.
     const inputProps = {
-      placeholder: 'Type a programming language'
-    }
-    return(
-      <div className="search-bar">
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-        />
-        <input type="text" className="search" placeholder="search runner's name"/>
-        <div className="filters">
-          <p>filter by:</p>
-          <a>all runners</a>
-          <a>St. Jude Heroes</a>
-          <a>Boston Qualifiers</a>
-          <a>Age group winners</a>
+      placeholder: 'Search for a runner',
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+        <div className="search-bar">
+
+          <Autosuggest
+              className="search"
+              suggestions={suggestions}
+              onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+              onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps}
+          />
+
+          <div className="filters">
+            <p>filter by:</p>
+            <a>all runners</a>
+            <a>St. Jude Heroes</a>
+            <a>Boston Qualifiers</a>
+            <a>Age group winners</a>
+          </div>
         </div>
-      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import ResultsTable from './ResultsTable'
+import SearchBar from './SearchBar'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
     <div className="app-container">
 
       <App/>
-
+      <SearchBar />
       <ResultsTable/>
     </div>
     , document.getElementById('root'));
