dc25c60d0ae772f0ac9891c2fbddeb8c5dd5ffb9
Trenton Kennedy <trentontri@gmail.com>
2018-11-01 21:13:37 -0500

    update server

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.set('view engine', 'pug');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Origin', 'https://memphis-marathon.herokuapp.com');
-  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
